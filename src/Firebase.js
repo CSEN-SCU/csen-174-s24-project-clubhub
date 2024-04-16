02598b14f6823c2371871976bdd387ca89ab9836
Justin Chung <jdchung@scu.edu>
2024-04-16 10:56:11 -0700

    Add temporary login

---
+++ src/Firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVcmO3ibqY6ipNsEENaU7r7nwsOMN5P6M",
+  authDomain: "clubhub-ca026.firebaseapp.com",
+  projectId: "clubhub-ca026",
+  storageBucket: "clubhub-ca026.appspot.com",
+  messagingSenderId: "1072190558895",
+  appId: "1:1072190558895:web:7745514191b0563739fa5a",
+  measurementId: "G-NVPCTP2SLM"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL;
+
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("profilePic", profilePic);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
