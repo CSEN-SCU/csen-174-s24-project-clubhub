245318961ed3e463c4bffcb861b0ea31586cb627
Justin Chung <jdchung@scu.edu>
2024-04-17 17:52:33 -0700

    add account info and logout button

---
+++ src/pages/account/account.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, firestore } from "../../Firebase";
+import { signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import "./account.css";
+
+function Account() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      localStorage.removeItem("userId");
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userId = localStorage.getItem("userId");
+      if (!userId) return;
+
+      const userRef = doc(firestore, "users", userId);
+      const docSnap = await getDoc(userRef);
+
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        setName(userData.name);
+        setEmail(userData.email);
+        setProfilePic(userData.profilePic || "");
+      } else {
+        console.log("No user data available");
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+  return (
+    <div className="container">
+      <h1>{name}</h1>
+      <h1>{email}</h1>
+      {profilePic && <img src={profilePic} alt="Profile" />}
+      <button onClick={handleLogout} className="logout-button">
+        Log Out
+      </button>
+    </div>
+  );
+}
+
+export default Account;
