c9a5a57c9e777c2c9282a9bef816d28d93142fed
meaganvu <mavu@scu.edu>
2024-04-28 16:38:07 -0700

    Adding Tag to PracticeArea and AcademicBackground

--- src/pages/clubs/Modal.js
+++ src/pages/clubs/Modal.js
@@ -1,6 +1,26 @@
-import React from 'react'
+// import React from 'react'
 import "./Modal.css"
+import React from 'react';
 //import { React, useState } from "react";
+
+function slugify(input) {
+    if (!input)
+        return '';
+
+    // make lower case and trim
+    var slug = input.toLowerCase().trim();
+
+    // remove accents from charaters
+    slug = slug.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+    // replace invalid chars with spaces
+    slug = slug.replace(/[^a-z0-9\s-]/g, ' ').trim();
+
+    // replace multiple spaces or hyphens with a single hyphen
+    slug = slug.replace(/[\s-]+/g, '-');
+
+    return slug;
+}
 
 function Modal({closeModal, clubInfo}){
     return(
@@ -11,6 +31,9 @@ function Modal({closeModal, clubInfo}){
         </div>
             <div className = "title">
                 <h1>{clubInfo.ClubName}</h1>
+                <div className = "ProfileButton">
+                <button>Club Profile Page</button>
+                </div>
             </div>
             <div className = "body">
 
@@ -20,33 +43,96 @@ function Modal({closeModal, clubInfo}){
                 </div>
                 <div id = "AcademicBackground" className = "component">
                     <h5>Academic Background</h5> 
-                    {/* for loop */}
-                    <p>{clubInfo.AcademicBackground}</p>
+                    <div className="tags">
+                        {clubInfo?.AcademicBackground?.map((word, index) => (
+                            <span key={index} className = {slugify(word)}>
+                            {word}
+                            </span>
+                        ))}
+                    </div>
                 </div>
                 <div id = "PracticeArea" className = "component">
                     <h5>Practice Area</h5>
-                    {/* for loop */}
-                    <p>{clubInfo.PracticeArea}</p>
+                    <div className="tags">
+                        {clubInfo?.PracticeArea?.map((word, index) => (
+                            <span key={index} className = {slugify(word)}>
+                            {word}
+                            </span>
+                        ))}
+                    </div>
                 </div>
 
-                <div id = "Contact" className ="component">
-                    <h5>Contact Name</h5>
-                    <p>{clubInfo.ContactName}</p>
-                    <h5>Contact Email</h5>
-                    <p>{clubInfo.Contact}</p>
+                <div className="twoStack">
+                    <div className="component" id = "ContactName">
+                        <h5>Contact Name</h5>
+                        <p>{clubInfo.ContactName}</p>
+                    </div>
+                    <div className="component" id = "ContactEmail">
+                        <h5>Contact Email</h5>
+                        <p>{clubInfo.Contact}</p>
+                    </div>
                 </div>
 
-                <div id = "Sites" className ="component">
-                    <h5>Website</h5>
-                    <a>{clubInfo.Website}</a>
-                    <h5>Chat Channel</h5>
-                    <a>{clubInfo.ChatChannel}</a>
+                <div className="twoStack">
+                    <div className="component" id = "Website">
+                        <h5>Website</h5>
+                        <a href={clubInfo.Website}> {clubInfo.Website} </a>
+                    </div>
+                    <div className="component" id = "ChatChannel">
+                        <h5>Chat Channel</h5>
+                        <a href = {clubInfo.ChatChannel}>{clubInfo.ChatChannel}</a>
+                    </div>
                 </div>
+
+                <div className="twoStack">
+                    <div className="component" id = "UnderOrGrad">
+                        <h5>Undergrate or Graduate</h5>
+                        <p>{clubInfo.UndergraduateorGraduate}</p>
+                    </div>
+                    <div className="component" id = "CurrentProjects">
+                        <h5>Current Projects</h5>
+                        <p>{clubInfo.CurrentProjects}</p>
+                    </div>
+                </div>
+
+                <div className="twoStack">
+                    <div className="component" id = "TimeCommitment">
+                        <h5>Time Commitment</h5>
+                        <p>{clubInfo.TimeCommitment}</p>
+                    </div>
+                    <div className="component" id = "RelaxedSerious">
+                        <h5>Relaxed or Seriousness</h5>
+                        <p>{clubInfo.RelaxedSerious}</p>
+                    </div>
+                </div>
+
+                <div className="twoStack">
+                    <div className="component" id = "MembershipFee">
+                        <h5>Membership Fee</h5>
+                        <p>{clubInfo.MembershipFee}</p>
+                    </div>
+                    <div className="component" id = "NationalOrganization">
+                        <h5>National Organization</h5>
+                        <p>{clubInfo.NationalOrganization}</p>
+                    </div>
+                </div>
+
+                <div className="twoStack">
+                    <div className="component" id = "IntroductionVideo">
+                        <h5>Introduction Video</h5>
+                        <p>{clubInfo.IntroductionVideo}</p>
+                    </div>
+                    <div className="component" id = "CoverPhoto">
+                        <h5>Cover Photo</h5>
+                        <p>{clubInfo.CoverPhoto}</p> 
+                        {/* Need to find how to access this */}
+                    </div>
+                </div>
+
+                
                 
             </div>
-            <div className = "footer">
-                <button>Club Profile Page</button>
-            </div>
+            
             </div>
             </div>
     )
