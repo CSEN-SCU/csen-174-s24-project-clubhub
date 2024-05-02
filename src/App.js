32e0ebfcf0f91538c62452a62f982041a626184a
Justin Chung <jdchung@scu.edu>
2024-05-02 11:11:00 -0700

    navigate to error page if error = scu email required

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/nav/navbar";
 import Welcome from "./pages/welcome/welcome";
+import Error from "./pages/error/error";
 import ClubPage from "./pages/clubs/ClubPage";
 import Home from "./pages/home/home";
 import GPT from "./pages/gpt/gpt";
@@ -11,8 +12,11 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 
 function PrivateRoute({ children }) {
-  const { currentUser } = useAuth();
-  return currentUser ? children : <Navigate to="/" />;
+  const { currentUser, error } = useAuth();
+
+  if (error && error === "SCU_EMAIL_REQUIRED") {
+    return <Navigate to="/error" />;
+  }  return currentUser ? children : <Navigate to="/" />;
 }
 
 function PublicRoute({ children }) {
@@ -24,51 +28,59 @@ function App() {
   return (
     <AuthProvider>
       <div id="appBody">
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PublicRoute>
-                <Welcome />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/clubs"
-            element={
-              <PrivateRoute>
-                <ClubPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/gpt"
-            element={
-              <PrivateRoute>
-                <GPT />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/account"
-            element={
-              <PrivateRoute>
-                <Account />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </div>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Welcome />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/error"
+              element={
+                <PublicRoute>
+                  <Error />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/clubs"
+              element={
+                <PrivateRoute>
+                  <ClubPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/gpt"
+              element={
+                <PrivateRoute>
+                  <GPT />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/account"
+              element={
+                <PrivateRoute>
+                  <Account />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
       </div>
     </AuthProvider>
   );
