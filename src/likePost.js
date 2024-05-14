ddd76ea9e51bac686efd9856f2abd62fa48150a1
etruongofficial <etruongofficial@gmail.com>
2024-05-14 14:57:34 -0700

    Like Posts Feature Initial

---
+++ src/likePost.js
@@ -0,0 +1,10 @@
+// likePost.js
+import { firestore } from './Firebase';
+import { doc, setDoc, FieldValue } from "firebase/firestore";
+
+const likePost = async (postId) => {
+  const postRef = doc(firestore, "posts", postId);
+  await setDoc(postRef, { likes: FieldValue.increment(1) }, { merge: true });
+};
+
+export default likePost;
