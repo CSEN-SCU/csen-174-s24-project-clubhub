992ed80657aac85490bb47afe283670779d20322
etruongofficial <etruongofficial@gmail.com>
2024-06-03 15:07:20 -0700

    Like Button

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -2,36 +2,36 @@ import React from "react";
 import "./post.css";
 import Avatar from "../../assets/sculogo.png";
 import { Link } from "react-router-dom";
+import LikeButton from "../../likeButton";
 
 const formatDate = (timestamp) => {
   let date;
   if (timestamp?.toDate) {
-    // Handle Firestore Timestamp objects
     date = timestamp.toDate();
   } else {
     date = new Date(timestamp);
   }
 
-  // Formatting the date part
   const optionsDate = { month: "short", day: "numeric" };
   const formattedDate = date.toLocaleDateString("en-US", optionsDate);
 
-  // Formatting the time part
   const optionsTime = { hour: "numeric", minute: "2-digit", hour12: true };
   const formattedTime = date.toLocaleTimeString("en-US", optionsTime);
 
-  // Combining both parts
   return `${formattedDate} • ${formattedTime}`;
 };
 
-function post({
+function Post({
+  postId, // Add postId prop
   displayName,
   timestamp,
   text,
   image,
   userID,
   title,
-  // avatar
+  likes, // Add likes prop
+  likedBy, // Add likedBy prop
+  currentUserId // Add currentUserId prop
 }) {
   return (
     <div className="post">
@@ -47,19 +47,22 @@ function post({
               <h2>{displayName}</h2>
             </Link>
             <p className="post__time">{formatDate(timestamp)}</p>
-            {/* <p className='post__time'>Feb 12 • 3:00 pm</p> */}
-            <h3>
-                {title}
-            </h3>
+            <h3>{title}</h3>
           </div>
           <div className="post__headerDesc">
             <p>{text}</p>
           </div>
         </div>
         <img className="post__image" src={image} alt="" />
+        <LikeButton 
+          postId={postId} // Pass postId to LikeButton
+          initialLikes={likes} // Pass initialLikes to LikeButton
+          initialLikedBy={likedBy} // Pass initialLikedBy to LikeButton
+          userId={currentUserId} // Pass currentUserId to LikeButton
+        />
       </div>
     </div>
   );
 }
 
-export default post;
+export default Post;
