7571b88f34f2891adc06257bf71bc8f2d114d246
Justin Chung <jdchung@scu.edu>
2024-06-09 12:10:46 -0700

    fix gpt height

--- src/pages/gpt/gpt.jsx
+++ src/pages/gpt/gpt.jsx
@@ -7,7 +7,7 @@ function Gpt() {
       <iframe
         src="http://localhost:8501"
         width="100%"
-        height="1000px"
+        height="100%"
         style={{ border: 'none' }}
         title="Clubo Chat-Bot"
       ></iframe>
